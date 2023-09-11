918b02720da5e550d6b43a1be2b9dcf1a7d31779
WalkerWest <walker.west@gmail.com>
2023-09-11 03:11:01 -0500

    Fix filesystem (Attempt #1)

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 /usr/bin/mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
 /usr/bin/screen -dmS session bash
+
 /usr/bin/screen -S session -p 0 -X stuff "/usr/libexec/mysqld --user mysql"
 updatedb
 cd /app/venue
 # /usr/bin/screen -d -m java -jar target/tea-1.0-SNAPSHOT-jar-with-dependencies.jar
-
 java -jar target/tea-1.0-SNAPSHOT-jar-with-dependencies.jar
