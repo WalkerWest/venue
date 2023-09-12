47050ab462f031a0189545c5c425ea211fbe8383
WalkerWest <walker.west@gmail.com>
2023-09-12 04:37:47 -0500

    Refactor for JDK 10; add first REST endpoint

--- src/main/java/nblc/rest/MyMessage.java
+++ src/main/java/nblc/rest/MyMessage.java
@@ -5,8 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import nblc.Reservation;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("")
 public class MyMessage {
@@ -16,6 +20,15 @@ public class MyMessage {
     @Path("msg") @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getMessage() { return "My message\n"; }
+
+    @Path("reservation") @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Reservation> getReservation() {
+        var retList = new ArrayList<Reservation>();
+        retList.add(new Reservation("Walker",4));
+        return retList;
+    }
+
 
 }
 
